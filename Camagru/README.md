9184e5f50fb2739d07c0a18d503e2af2d8bbe581
Vasco Eti <veti@c3r8s1.wethinkcode.co.za>
2019-11-14 17:23:08 +0200

    camagru

---
+++ Camagru/README.md
@@ -0,0 +1,3 @@
+Camagru is an Instagram-like website. It allows you to take pictures with your webcam or upload one, and add beautiful filters on it.
+
+It is the first Web Project for WeThinkCode_.
---
+++ Camagru/config/db_init.php
@@ -0,0 +1,15 @@
+<?php
+	include ("config/database.php");
+	try {
+		$dbh = new PDO($dsn, $db_user, $db_passwd);
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$dbh->exec("CREATE DATABASE IF NOT EXISTS $db_name");
+	}
+	catch(PDOException $e) {
+		echo "ERROR: ".$e->getMessage();
+		exit(2);
+	}
+// $dbh = NULL;
+// if ($dbh)
+// 	echo "success!\n";
+?>
